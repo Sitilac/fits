2e6a4382eaa712211e236081740c1d2a4cdb5b53
Sitilac <mcalitis@MacBook-Pro.local>
2020-10-13 16:53:23 -0700

    Modified AddTop

--- src/pages/AddTopPage/AddTopPage.jsx
+++ src/pages/AddTopPage/AddTopPage.jsx
@@ -15,7 +15,6 @@ class AddTopPage extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    this.props.handleAddPuppy(this.state.formData);
   };
 
   handleChange = e => {
@@ -29,45 +28,6 @@ class AddTopPage extends Component {
   render() {
     return (
       <>
-        <h1>Add Puppy</h1>
-        <form ref={this.formRef} autoComplete="off" onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label>Pup's Name (required)</label>
-            <input
-              className="form-control"
-              name="name"
-              value={this.state.formData.name}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label>Pup's Breed (required)</label>
-            <input
-              className="form-control"
-              name="breed"
-              value={this.state.formData.breed}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label>Pup's Age</label>
-            <input
-              className="form-control"
-              name="age"
-              value={this.state.formData.age}
-              onChange={this.handleChange}
-            />
-          </div>
-          <button
-            type="submit"
-            className="btn"
-            disabled={this.state.invalidForm}
-          >
-            ADD PUPPY
-          </button>
-        </form>
       </>
     );
   }
